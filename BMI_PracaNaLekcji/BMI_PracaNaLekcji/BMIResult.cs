389eb9f26087a49888379ec088b197c490bb7847
patryk.labuz <patryk.labuz@S50-11>
2024-02-28 09:26:53 +0100

    Utworzenie front-endu stron oraz klasy BMIResult

--- BMI_PracaNaLekcji/BMI_PracaNaLekcji/BMIResult.cs
+++ BMI_PracaNaLekcji/BMI_PracaNaLekcji/BMIResult.cs
@@ -6,6 +6,47 @@ namespace BMI_PracaNaLekcji
 {
     public class BMIResult
     {
+        private static int LastId;
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public DateTime Date { get; set; }
+        public int Weight { get; set; }
+        public int Height { get; set; }
+        public string Gender { get; set; }
+        public float Score { get; set; }
+        public string Result { get; set; }
 
+
+        public BMIResult(int id, string title, DateTime date, int weight, int height, string gender, float score, string result)
+        {
+            Id = id;
+            Title = title;
+            Date = date;
+            Weight = weight;
+            Height = height;
+            Gender = gender;
+            Score = score;
+            Result = result;
+        }
+        public BMIResult(string title, DateTime date, int weight, int height, string gender, float score, string result)
+        {
+            LastId++;
+            Id = LastId;
+            Title = title;
+            Date = date;
+            Weight = weight;
+            Height = height;
+            Gender = gender;
+            Score = score;
+            Result = result;
+        }
+        public BMIResult()
+        {
+
+        }
+        public void SetLastId()
+        {
+            LastId = Id;
+        }
     }
 }
